2129fb2d496e7e54afee1b080cb6b1ed4a25c797
conor <clmurphy@sfu.ca>
2021-12-23 20:31:55 -0500

    make matching summary look kind of usable

--- client/src/components/responseMatching/MatchingSummary.vue
+++ client/src/components/responseMatching/MatchingSummary.vue
@@ -1,14 +1,16 @@
 <template>
-  <h3> {{roundPoints}} </h3>
-  <div class="list-group w-100 h-100">
-    <div v-for="match in matches"
-         class="list-group-item">
-      {{ match.player.name }}: {{match.response}}
+  <div class="list-group w-100 h-100 align-items-center">
+    <h3 class="m-5"> {{ roundPoints }} </h3>
+    <div class="list-group w-75 h-100">
+      <div v-for="match in matches"
+           class="list-group-item">
+        {{ match.player.name }}: {{ match.response ? 'Strike!' : 'Sike!'}}
+      </div>
     </div>
+    <button class="btn btn-primary w-50 fs-4 m-5"
+            :class="{'d-none': !canEndRound}" @click="endRound">Next Person
+    </button>
   </div>
-  <button class="btn btn-primary w-50 fs-4"
-          :class="{'d-none': !canEndRound}" @click="endRound">Next Person
-  </button>
 </template>
 
 <script>

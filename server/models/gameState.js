797ba18abc9a281212e1c8b0e76c020c6bc4a637
conor <clmurphy@sfu.ca>
2021-12-27 23:02:08 -0500

    turn feature off by default

--- server/models/gameState.js
+++ server/models/gameState.js
@@ -4,7 +4,7 @@ const defaultOptions = () => {
     return {
         promptTimer: 45,
         numRounds: 8,
-        sikeDispute: true,
+        sikeDispute: false,
         sikeRetries: 1,
     }
 }

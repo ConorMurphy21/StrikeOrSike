8e98cabaa0675a3f70152089dadbc53351763fd8
Conor Murphy <38164654+ConorMurphy21@users.noreply.github.com>
2022-01-10 17:15:01 -0800

    * Style lobby (#47)

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,6 @@
   <div class="container vh-100 d-flex flex-column justify-content-top align-items-center">
     <img class="my-3" src="@/assets/logo.png" alt="Strike Or Sike">
     <router-view/>
-    <img class="my-3 d-none" src="@/assets/logo.png" alt="Strike Or Sike">
-      <!-- A really jenky way to approximately center the router-->
   </div>
 </template>
 

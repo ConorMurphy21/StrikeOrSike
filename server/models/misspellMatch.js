a02d8d4d810c29e853329ed44f561a1732ce2abf
Conor Murphy <38164654+ConorMurphy21@users.noreply.github.com>
2022-01-20 23:52:49 -0800

    Auto match (#52)
    * * auto-match everything except misspellings
    
    * * add spell check but it's slow. So I should probably make it async
    
    * improve spellcheck
    
    Co-authored-by: conor <clmurphy@sfu.ca>

---
+++ server/models/misspellMatch.js
@@ -0,0 +1,31 @@
+const Spellchecker = require('spellchecker');
+const en = new Spellchecker.Spellchecker();
+en.setSpellcheckerType(Spellchecker.ALWAYS_USE_HUNSPELL);
+en.setDictionary('en_CA', './resources/dictionaries');
+spellcheckers = {
+    en
+}
+module.exports = (string1, string2, lang) => {
+    if(string1.includes(' ') || string2.includes(' ')) return 0;
+    const spellchecker = spellcheckers[lang] ?? en;
+    const mis1 = spellchecker.isMisspelled(string1);
+    const mis2 = spellchecker.isMisspelled(string2);
+    if(mis1 && mis2){
+        const options1 = spellchecker.getCorrectionsForMisspelling(string1);
+        const options2 = spellchecker.getCorrectionsForMisspelling(string2);
+        const intersection = options1.filter(element => options2.includes(element));
+        let percent = 1;
+        for(const element of intersection){
+            const ind = Math.max(options1.indexOf(element), options2.indexOf(element));
+            percent *= 0.15 * (ind/3);
+        }
+        return 1 - percent;
+    } else if(mis1 || mis2){
+        const mis = mis1 ? string1 : string2;
+        const correct = mis1 ? string2 : string1;
+        const options = spellchecker.getCorrectionsForMisspelling(mis);
+        const index = options.indexOf(correct);
+        return index === undefined ? 0 : 0.95 - index * 0.03;
+    }
+    return 0;
+}

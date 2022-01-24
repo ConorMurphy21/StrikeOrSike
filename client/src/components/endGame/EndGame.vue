20189844388ea0db7bbd2906d4bc0f847be90951
conor <conormurphyx@gmail.com>
2022-01-23 20:11:21 -0800

    * somewhat style end-game

--- client/src/components/endGame/EndGame.vue
+++ client/src/components/endGame/EndGame.vue
@@ -1,11 +1,16 @@
 <template>
- <div class="w-100 d-flex flex-column justify-content-start align-items-center gap-2 my-3">
-   <div v-for="(score, index) in scores">
-     {{index + 1}}. {{score.player.name }} : {{$n(score.points)}}
-   </div>
+  <div class="w-100 h-100 d-flex flex-column justify-content-start align-items-center gap-3">
+    <h1 class="mt-2" v-t="'playerScores'"/>
 
-   <button class="btn btn-orange" @click="setScene('lobby')">Back to Lobby</button>
- </div>
+    <div class="list-group w-50">
+      <div v-for="(score, index) in scores" class="list-group-item">
+        {{ $n(index + 1) }}. {{ score.player.name }}: {{ $n(score.points) }}
+      </div>
+    </div>
+
+    <div class="flex-grow-1"/>
+    <button class="btn btn-blue mb-5" @click="setScene('lobby')">Back to Lobby</button>
+  </div>
 </template>
 
 <script>
@@ -21,12 +26,24 @@ export default {
   },
   methods: {
     ...mapMutations([
-        'setScene'
+      'setScene'
     ])
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+h1 {
+  font-family: $header-font !important;
+  font-size: 4rem;
+  font-weight: normal;
+  color: $red;
+}
+
+.btn {
+  width: 30%;
+  min-width: 150px;
+  height: 60px;
+}
 </style>

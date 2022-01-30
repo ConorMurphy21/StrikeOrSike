cf651ed2595169151b90bb910bc35ca56a141227
conor <conormurphyx@gmail.com>
2022-01-30 03:05:28 -0800

    * change node.js to always serve index instead of just when it is an exact match

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const logger = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+app.get('/*', function(req, res){
+    res.sendFile( path.join(__dirname, 'public/index.html'));
+});
 
 module.exports = app;

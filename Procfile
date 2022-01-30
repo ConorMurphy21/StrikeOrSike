73e7806e879266439b1b0770146a8b717d090abc
conor <conormurphyx@gmail.com>
2022-01-30 02:10:43 -0800

    * delete nginx config because most of that should be able to be done in aws  * make express.js serve static public directory  * make build move client static app to server public folder  * only keep necessary artifacts in build  * move nodejs to top level instead of creating a Procfile  * add ebextensions to hopefully get the static hosting performance that nginx has to offer

--- Procfile
+++
@@ -1,2 +0,0 @@
-web_client: cd client && npm run serve
-web_server: cd server && npm run start
--- server/app.js
+++ server/app.js
@@ -3,14 +3,12 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
-const indexRouter = require('./routes/api');
-
 const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use('/api', indexRouter);
+app.use(express.static('public'));
 
 module.exports = app;

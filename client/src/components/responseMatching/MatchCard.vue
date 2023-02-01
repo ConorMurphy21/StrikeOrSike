a1e026457c2649e2e2c4716e7ecbfcae0026a02f
conor <conormurphyx@gmail.com>
2023-01-31 23:15:46 -0800

    * give unmatch button the action of redirecting the player to the activeMatching screen

--- client/src/components/responseMatching/MatchCard.vue
+++ client/src/components/responseMatching/MatchCard.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="root">
-    <h1 v-if="match && match.response" class="fs-3"> {{ match.response }} </h1>
-    <img v-else-if="match" class="fs-3" src="@/assets/images/sike.png" :alt="$t('sike')"/>
-    <div v-else class=" p-3 d-flex justify-content-center align-items-center position-relative">
-      <div class="dot-pulse"/>
+    <div class="d-flex flex-row w-100 gap-1 justify-content-center align-items-center"
+         @mouseenter="hovering=true" @mouseleave="hovering=false">
+      <h1 v-if="match && match.response" class="fs-3"> {{ match.response }} </h1>
+      <img v-else-if="match" class="fs-3" src="@/assets/images/sike.png" :alt="$t('sike')"/>
+      <div v-else class=" p-3 d-flex justify-content-center align-items-center position-relative">
+        <div class="dot-pulse"/>
+      </div>
+      <a @click="unmatch" v-if="showUnmatch"><i class="bi-trash3-fill text-red fw-bolder fs-4"/></a>
     </div>
     <h2 class="fs-5">{{ player.name }}</h2>
   </div>
 </template>
 
 <script>
+import {createNamespacedHelpers} from 'vuex';
+
+const room = createNamespacedHelpers('room');
+const game = createNamespacedHelpers('game');
 
 export default {
   props: {
     player: Object,
     match: Object
   },
+  data() {
+    return {
+      hovering: false
+    }
+  },
+  computed: {
+    ...room.mapState([
+      'name'
+    ]),
+    showUnmatch() {
+      return this.player.name === this.name && this.hovering;
+    }
+  },
+  methods: {
+    ...game.mapActions([
+        'unmatch'
+    ]),
+  }
 }
 </script>
 
@@ -26,13 +52,13 @@ export default {
 }
 
 h1 {
-  max-width: 150px;
   font-weight: bolder;
   text-align: center;
   text-overflow: ellipsis;
   overflow: hidden;
   color: $orange;
-  margin: auto;
+  margin-bottom: auto;
+  margin-top: auto;
   white-space: nowrap;
 }
 

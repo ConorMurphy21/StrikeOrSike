7749cbe466af9714af2b03ff5b1b2d2daf2f92ad
conor <conormurphyx@gmail.com>
2023-02-26 17:00:42 -0500

    * first attempt at adding UI element to toggle player prompts

--- client/src/components/endRound/EndRound.vue
+++ client/src/components/endRound/EndRound.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-100 d-flex flex-column justify-content-between align-items-center py-3 px-4">
     <prompt :prompt="prompt"/>
-    <response-list :selectable="false" :height="43"/>
+    <response-list :selectable="false" :height="43" :player-id="responsesId"/>
+    <player-chooser class="mb-2" v-model="responsesId"/>
     <button class="btn btn-orange w-75 w-lg-50 w-xl-25 fs-4 mb-3 position-relative"
             :class="{'btn-blue': !startNextRoundNext}"
          @click="sendVote">
       {{ hasNextRound ? $t('startNextRound') : $t('viewResults')}}
-
       <notification-count v-if='startNextRoundCount' class="position-absolute top-0 start-100 translate-middle fs-6">
         {{ $n(startNextRoundCount) }}
       </notification-count>
@@ -20,11 +20,18 @@ import Prompt from '@/components/gameShared/Prompt.vue';
 import ResponseList from '@/components/gameShared/ResponseList.vue';
 import NotificationCount from '@/components/gameShared/NotificationCount.vue';
 import ClickMp3 from '@/assets/audio/click2.mp3';
+import PlayerChooser from '@/components/endRound/PlayerChooser.vue';
 
 const {mapState, mapGetters} = createNamespacedHelpers('game');
 
 export default {
+  data(){
+    return {
+      responsesId: ''
+    }
+  },
   components: {
+    PlayerChooser,
     Prompt,
     ResponseList,
     NotificationCount
---
+++ client/src/components/endRound/PlayerChooser.vue
@@ -0,0 +1,25 @@
+<template>
+  <select class="form-select-lg">
+    <option v-for="player in players" :selected="player.id === self.id" :value="player.id">
+      {{player.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+
+import {createNamespacedHelpers} from 'vuex';
+const {mapState, mapGetters} = createNamespacedHelpers('room');
+
+export default {
+  computed: {
+    ...mapState([
+        'players'
+    ]),
+    ...mapGetters([
+        'self'
+    ])
+  },
+  emits: ['update:modelValue'],
+}
+</script>

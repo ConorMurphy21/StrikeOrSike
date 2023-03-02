a34d1c49ffe9cb93d029589243f2d5d8a0fd33e3
conor <conormurphyx@gmail.com>
2023-03-02 01:03:56 -0800

    * fix v-select width bug  * start to style v-select

--- client/src/components/endRound/PlayerChooser.vue
+++ client/src/components/endRound/PlayerChooser.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-select v-model="value" :options="players" label="name" :reduce="(option) => option.id"
-            :searchable="false" :clearable="false" />
+  <v-select class="text-center"
+      v-model="value" :options="players" :reduce="(option) => option.id"
+            :searchable="false" :clearable="false">
+    <template v-slot:option="player">
+      <span class="d-flex justify-content-center w-100">{{ player.name }}</span>
+    </template>
+  </v-select>
 </template>
 
 <script>
@@ -8,6 +13,9 @@ import {createNamespacedHelpers} from 'vuex';
 const {mapState, mapGetters} = createNamespacedHelpers('room');
 
 export default {
+  components: {
+    'v-select': window["vue-select"]
+  },
   computed: {
     ...mapState([
         'players'

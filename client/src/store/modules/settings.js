3c22b7992ecc68c7a4b94eac9dbf7eaf48cc11f8
conor <conormurphyx@gmail.com>
2023-04-15 02:05:15 -0700

    * add button to toggle tooltips

--- client/src/store/modules/settings.js
+++ client/src/store/modules/settings.js
@@ -2,12 +2,16 @@ const state = () => ({
     volume: 1,
     // used for muting and unmuting
     lastVolume: 1,
+    showTooltips: true
 });
 
 const mutations = {
     setVolume(state, data) {
         state.lastVolume = state.volume;
         state.volume = data;
+    },
+    setShowTooltips(state, data) {
+        state.showTooltips = data;
     },
     toggleMute(state) {
         if(state.volume) {

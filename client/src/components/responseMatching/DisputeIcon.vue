042625d4b32ddc338707e5e0c5716e8a05e491ba
Conor <conormurphyx@gmail.com>
2023-12-29 18:58:38 -0500

    * make disputeIcon look disabled properly

--- client/src/components/responseMatching/DisputeIcon.vue
+++ client/src/components/responseMatching/DisputeIcon.vue
@@ -1,30 +1,31 @@
 <template>
-  <a class="btn btn-sm btn-orange text-white ratio-1x1 position-relative d-inline-flex justify-content-center align-items-center"
-     :class="{'btn-blue': !sikeDisputeNext}"
-     :disabled="disabled"
-     v-tooltip="{title: $t('tooltip.dispute', {response}), placement}"
-     @click="sendVote">
-    <i class="bi-hand-thumbs-down fs-5 lh-sm"/>
+  <button
+    class="btn btn-sm btn-orange text-white ratio-1x1 position-relative d-inline-flex justify-content-center align-items-center"
+    :class="{'btn-blue': !sikeDisputeNext}"
+    :disabled="disabled"
+    v-tooltip="{title: $t('tooltip.dispute', {response}), placement}"
+    @click="sendVote">
+    <i class="bi-hand-thumbs-down fs-5 lh-sm" />
 
     <notification-count :width="21" v-if="sikeDisputeCount" class="position-absolute top-0 start-100 translate-middle">
       {{ $n(sikeDisputeCount) }}
     </notification-count>
-  </a>
+  </button>
 </template>
 
 <script>
-import {createNamespacedHelpers} from 'vuex';
+import { createNamespacedHelpers } from 'vuex';
 import NotificationCount from '@/components/gameShared/NotificationCount.vue';
 import ClickMp3 from '@/assets/audio/click2.mp3';
-import {AudioWrap} from '@/mixins/audiowrap';
+import { AudioWrap } from '@/mixins/audiowrap';
 
-const {mapGetters, mapState} = createNamespacedHelpers('game');
+const { mapGetters } = createNamespacedHelpers('game');
 
 export default {
   data() {
     return {
       tooltips: []
-    }
+    };
   },
   components: {
     NotificationCount
@@ -46,5 +47,12 @@ export default {
       this.$socket.emit('pollVote', 'sikeDispute');
     }
   }
-}
+};
 </script>
+
+<style>
+:focus:not(:focus-visible) {
+  outline: 0;
+  box-shadow: none;
+}
+</style>
